8d36e0c9f22e55b8b37eb9d9ad395d7b10adbdd1
Michal Takac <takacmichal92@gmail.com>
2015-09-14 22:15:56 +0200

    using Meteor version 1.1.0.3 apis

--- package.js
+++ package.js
@@ -1,19 +1,20 @@
 Package.describe({
-    name:"natestrauser:select2",
-    summary: "Select2 is a jQuery based replacement for select boxes.",
+  name:"natestrauser:select2",
+  summary: "Select2 is a jQuery based replacement for select boxes.",
   version: "4.0.0",
   git: "https://github.com/nate-strauser/meteor-select2.git"
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.0");
-    api.use("jquery", "client");
+  api.versionsFrom("METEOR@1.1.0.3");
+  
+  api.use("jquery", "client");
 
-    api.add_files([
-        "lib/select2/dist/js/select2.js"
-    ], "client", {bare: true});
+  api.add_files([
+    "lib/select2/dist/js/select2.js"
+  ], "client", {bare: true});
 
-    api.add_files([
-        "lib/select2/dist/css/select2.css"
-    ], "client");
+  api.add_files([
+    "lib/select2/dist/css/select2.css"
+  ], "client");
 });
